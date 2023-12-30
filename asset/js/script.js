a504e15786ab07d9904716c716e779a412a3d7ec
Tiamiyu Idris <tiamiyuidris2014@gmail.com>
2023-12-30 16:59:42 +0100

    forth commit

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,19 +1,3 @@
-
-
-// document.getElementsByTagName("body").addEventListener("scroll", function(){
-// 	let nav = document.getElementById("nav-tag");
-// 	let brand = document.querySelector(".brand");
-// 	let linkNav = document.querySelector(".nav-link");
-// 	let callIcon = document.getElementById("call");
-// 	let phoneNumber = document.getElementById("phone-num");
-// 	brand.style.color = "#000";
-// 	linkNav.style.color = "#000";
-// 	callIcon.style.backgroundColor = "#4da178";
-// 	phoneNumber.style.color = "#4da178";
-// 	phoneNumber.style.border = "think solid #4da178";
-// 	nav.style.backgroundColor = "#fff";
-// 	nav.style.borderRadius = "20px";
-// });
 let openIcon = document.querySelector(".menuIcon");
 let menuLink = document.querySelector(".nav-menu");
 let closeIcon = document.querySelector(".close-icon")
@@ -27,26 +11,37 @@ closeIcon.addEventListener("click", ()=>{
 })
 
 
-function nav(){
+
+let nav = document.getElementById("nav-tag");
+const navHeight = nav.getBoundingClientRect().height;
+let windowHeight = window.pageYOffset;
+let call = document.querySelector(".call");
+let phone_num = document.querySelector(".phone-num");
+
+window.addEventListener("scroll", function(){
 	let nav = document.getElementById("nav-tag");
-	let brand = document.querySelector(".brand");
-	let linkNav1 = document.querySelector(".nav-link1");
-	let linkNav2 = document.querySelector(".nav-link2");
-	let linkNav3 = document.querySelector(".nav-link3");
-	let linkNav4 = document.querySelector(".nav-link4");
-	let callIcon = document.getElementById("call");
-	let phoneNumber = document.getElementById("phone-num");
-	brand.style.color = "#000";
-	linkNav1.style.color = "#000";
-	linkNav2.style.color = "#000";
-	linkNav3.style.color = "#000";
-	linkNav4.style.color = "#000";
-	callIcon.style.backgroundColor = "#4da178";
-	phoneNumber.style.color = "#4da178";
-	phoneNumber.style.border = "3px solid #4da178";
-	nav.style.backgroundColor = "#fff";
-	nav.style.borderRadius = "10px";
-}
+	const navHeight = nav.getBoundingClientRect().height;
+	let windowHeight = window.pageYOffset;
+	let checkNav = nav.classList.contains("nav-class"); 
+	console.log(windowHeight)
+	if(windowHeight > navHeight){
+		nav.classList.add("nav-class");
+		//call.classList.add("call-color")
+		//console.log("hi")
+	}else{
+		//console.log("remove")
+		nav.classList.remove("nav-class")
+		//call.classList.remove("call-color")
+	}
+
+	if(checkNav){
+
+	}
+
+
+
+})
+
 
 //document.getElementsByTagName('body').addEventListener("scroll", nav)
 
